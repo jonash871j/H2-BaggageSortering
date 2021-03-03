f8d0449aa7dbca97ce9a87657f53c9f875f25cc4
jonash871j <55685252+jonash871j@users.noreply.github.com>
2021-03-03 16:26:22 +0100

    Commit

--- H2-BaggageSortering/Program.cs
+++ H2-BaggageSortering/Program.cs
@@ -1,85 +1,112 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using BaggageSorteringLib;
+﻿using BaggageSorteringLib;
 using Engine;
 
 class Program
 {
-    static Random rng = new Random();
-    static string sortingMachineInfo = "";
+    static ConsoleBuffer infoBuffer = new ConsoleBuffer(12);
+
     static void Main(string[] args)
     {
         Simulator simulator = new Simulator();
         simulator.SortingMachine.ProcessInfo += OnSortingMachineProcessInfo;
+        simulator.SortingMachine.ProcessException += OnSortingMachineProcessException;
         simulator.Start();
 
-        ConsoleEx.Create(128, 32);
+        ConsoleEx.Create(80, 32);
         ConsoleEx.SetFont("Consolas", 8, 16);
 
         while (true)
         {
-            for (int i = 0; i < simulator.Counters.Length; i++)
-            {
-                ConsoleEx.WriteLine($"Counter {simulator.Counters[i].Id}: IsReady {simulator.Counters[i].IsReady()}");
-            }
+            UserInput(simulator);
 
-            if (Input.KeyPressed((Key)'A'))
-            {
-                simulator.Counters[0].Open();
-                simulator.Counters[0].CheckLuggageIn(new Luggage("Tobias", 0));
-            }
-            if (Input.KeyPressed((Key)'B'))
-            {
-                simulator.Counters[1].Open();
-                simulator.Counters[1].CheckLuggageIn(new Luggage("Tobias", 1));
-            }
-            if (Input.KeyPressed((Key)'C'))
-            {
-                simulator.Counters[2].Open();
-                simulator.Counters[2].CheckLuggageIn(new Luggage("Tobias", 2));
-            }
-
-            DrawTray(simulator.SortingMachine.BigTrayFromCounters, "BigTrayFromCounters", 16);
-            DrawTerminals(simulator.Terminals, 17);
-
-
-            ConsoleEx.WriteLine();
-            ConsoleEx.WriteLine();
-            ConsoleEx.WriteLine(sortingMachineInfo);
+            DrawCounters(simulator.Counters, 1);
+            DrawConveyorBelt(simulator.SortingMachine.ConveyorBelt, " - Sorting system's conveyor belt", 8);
+            DrawTerminals(simulator.Terminals, 9);
+            DrawInfoBuffer();
 
             ConsoleEx.Update();
             ConsoleEx.Clear();
         }
     }
 
-    private static void OnSortingMachineProcessInfo(string msg)
+    // User input ***********************************
+    private static void UserInput(Simulator simulator)
     {
-        sortingMachineInfo = msg;
+        if (Input.KeyPressed((Key)'1'))
+        {
+            simulator.Counters[0].CheckLuggageIn(new Luggage("Name 1", 1));
+        }
+        if (Input.KeyPressed((Key)'2'))
+        {
+            simulator.Counters[1].CheckLuggageIn(new Luggage("Name 2", 2));
+        }
+        if (Input.KeyPressed((Key)'3'))
+        {
+            simulator.Counters[2].CheckLuggageIn(new Luggage("Name 3", 3));
+        }
+        if (Input.KeyPressed((Key)'4'))
+        {
+            simulator.Counters[3].CheckLuggageIn(new Luggage("Name 4", 4));
+        }
+        if (Input.KeyPressed((Key)'5'))
+        {
+            simulator.Counters[4].CheckLuggageIn(new Luggage("Name 5", 5));
+        }
     }
 
-    static void DrawTray(BufferTray<Luggage> tray, string name, int y)
+    // Events ***************************************
+    private static void OnSortingMachineProcessInfo(string msg)
     {
-        for (int i = 0; i < tray.Position; i++)
-        {
-            Luggage luggage = tray.Buffer[i];
+        infoBuffer.WriteLine(msg);
+    }
+    private static void OnSortingMachineProcessException(string msg)
+    {
+        infoBuffer.WriteLine("\fc" + msg);
+    }
 
-            ConsoleEx.WriteCharacter(i, y, '■', (byte)(luggage.TerminalId + 1));
+    // Draw functions *******************************
+    static void DrawConveyorBelt(ConveyorBelt<Luggage> conveyorBelt, string name, int y)
+    {
+        for (int i = 0; i < conveyorBelt.Length; i++)
+        {
+            Luggage luggage = conveyorBelt.Buffer[i];
+
+            if (luggage != null)
+            {
+                ConsoleEx.WriteCharacter(i, y, '■', (byte)(luggage.TerminalId + 1));
+            }
         }
-        ConsoleEx.WriteCoord(tray.Position + 3, y, name);
+        ConsoleEx.SetPosition(conveyorBelt.Length + 3, y);
+        ConsoleEx.WriteLine(name);
+    }
+    static void DrawCounters(Counter[] counters, int y)
+    {
+        ConsoleEx.SetPosition(0, y);
+        foreach (Counter counter in counters)
+        {
+            ConsoleEx.WriteLine($"Counter {counter.Id}: IsReady {counter.IsReady()}");
+        }
     }
     static void DrawTerminals(Terminal[] terminals, int y)
     {
         for (int i = 0; i < terminals.Length; i++)
         {
             Terminal terminal = terminals[i];
+
+            ConsoleEx.SetPosition(0, y + i);
+            ConsoleEx.WriteLine($"Terminal {terminal.Id} \f{terminal.Id} = ");
+
             int j = 0;
             foreach (Luggage luggage in terminal.Luggages)
             {
-                ConsoleEx.WriteCharacter(j, y + i, '■', (byte)(luggage.TerminalId + 1));
+                ConsoleEx.WriteCharacter(j + 14, y + i, '■', (byte)(terminal.Id));
                 j++;
             }
-            ConsoleEx.WriteCoord(terminal.Luggages.Count + 3, y + i, $"Terminal {terminal.Id}");
         }
+    }
+    static void DrawInfoBuffer()
+    {
+        Draw.Rectangle(0, 18, ConsoleEx.Width - 1, ConsoleEx.Height - 1, true, '.');
+        Draw.ConsoleBuffer(2, 19, infoBuffer);
     }
 }
