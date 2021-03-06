ef2e0adf9d641810d8773e351e18708c68bd6e34
Jonas hofmann <Jonash871j@gmail.com>
2021-03-06 09:40:15 +0100

    Commit

--- BaggageSortering/Simulator.cs
+++ BaggageSortering/Simulator.cs
@@ -86,7 +86,6 @@ namespace BaggageSorteringLib
 
                             if (reservation != null)
                             {
-                                // TODO: Fix gate
                                 CheckLuggageIn(counter.Id, new Luggage(reservation.Flight.TerminalId, counter.Id, reservation));
                             }
                         }
