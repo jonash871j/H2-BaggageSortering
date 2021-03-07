39aab1437a6331260d2688f23c5123631563bec1
Jonas hofmann <Jonash871j@gmail.com>
2021-03-07 14:21:37 +0100

    Commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ class Program
 
     static void Main()
     {
-        // Initialize simulator
+        //  Initialize simulator
         Simulator sim = new Simulator(
             counterAmount: 10,
             terminalAmount: 15,
